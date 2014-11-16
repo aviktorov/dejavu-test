5e5ff160ed2759aab25e3e5226df3abf23996233
Andrey Viktorov <aviktorov@fatestudio.org>
2014-11-16 18:40:21 +0600

    Project

---
+++ Source/DejaVu/DejaVuGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DejaVu.h"
+#include "DejaVuGameMode.h"
+
+
+ADejaVuGameMode::ADejaVuGameMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/DejaVu/DejaVuGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DejaVuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEJAVU_API ADejaVuGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
